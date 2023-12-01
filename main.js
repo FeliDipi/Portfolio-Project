3e1c0ad6201a1f96ba68b6458b3afba36e7e56df
NicoDipi <nicolas.dipierro@7r1ck.com>
2023-11-30 23:16:39 -0300

    +Reveal Effect added *Company Section Position

--- main.js
+++ main.js
@@ -3,8 +3,13 @@ import { Navbar, UpdateSection } from "./src/scripts/Navbar.js";
 import { Work } from "./src/Scripts/Work.js";
 import { Contact } from "./src/Scripts/Contact.js";
 
+import { Reveal } from "./src/scripts/Reveal.js";
+
+Reveal();
+
 window.onscroll = () =>
 {
+    Reveal();
     UpdateSection();
 }
 
